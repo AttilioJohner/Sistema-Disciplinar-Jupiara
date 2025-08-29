449713354e73d472ed41441f9507d60d2979bab0
AttilioJohner <ajtachinardi@gmail.com>
2025-08-29 16:35:01 -0400

    Update supabaseClient.js

--- scripts/supabaseClient.js
+++ scripts/supabaseClient.js
@@ -1,6 +1,12 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-const env = (window.__ENV||{});
-if(!env.SUPABASE_URL||!env.SUPABASE_ANON_KEY){
-  console.warn("Supabase env ausente em config.js");
+
+const env = (window.__ENV || {});
+if (!env.SUPABASE_URL || !env.SUPABASE_ANON_KEY) {
+  console.warn("Supabase env ausente em config.js / env-config.js");
 }
-export const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
+
+// Garante apenas 1 cliente Supabase no navegador
+if (!window.__supabase) {
+  window.__supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
+}
+export const supabase = window.__supabase;
